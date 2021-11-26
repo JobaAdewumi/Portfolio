dafd3e9cba68342b73abb327e91b4199144c6805
JobaAdewumi <94983678+JobaAdewumi@users.noreply.github.com>
2021-11-26 21:17:58 +0100

    Add files via upload

---
+++ Portfolio/main.js
@@ -0,0 +1,6 @@
+const menuIconContainer = document.querySelector("nav .menu-icon-container");
+const navContainer = document.querySelector(".nav-container");
+
+menuIconContainer.addEventListener("click", () => {
+    navContainer.classList.toggle("active");
+})
